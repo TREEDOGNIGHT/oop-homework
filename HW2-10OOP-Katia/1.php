37fadca1403c07eb3ed0aaf30d7b42d70de00856
nazar <nazar.mykhalus@gmail.com>
2021-11-12 12:46:34 +0200

    add 3,4,5

--- HW2-10OOP-Katia/1.php
+++ HW2-10OOP-Katia/1.php
@@ -1,9 +1,8 @@
 <?php
 class President {
-  // Property Declaration and Definition - notice the "public" keyword before the definition
+
   public $name = "Barack Obama";
 
-  // Method Definition - again, notice the "public" keyword before the actual definition
   public function greet($name) {
     return "Hello $name, my name is Barack Obama, nice to meet you!";
   }
---
+++ HW2-10OOP-Katia/3.php
@@ -0,0 +1,6 @@
+class CurrentUSPresident {
+  const name = "Barack Obama";
+  public static function greet($name) {
+    return "Hello $name, my name is Barack Obama, nice to meet you!";
+  }
+}
