39d0dba47bca9c2fe691c8c01dc77e54139b8b55
nazar <nazar.mykhalus@gmail.com>
2022-02-17 02:05:39 +0200

    Change hw 9 + add Utest

--- HW8-Improve-Text-Analyzer/index.php
+++ HW8-Improve-Text-Analyzer/index.php
@@ -1,102 +1,188 @@
-<style>
-    body {
-        width: 610px;
-    }
+<?php
 
-    #frmContact {
-        border-top: #F0F0F0 2px solid;
-        background: #FAF8F8;
-        padding: 10px;
-    }
+header('Content-Type: text/html; charset=utf-8');
 
-    #frmContact div {
-        margin-bottom: 15px
-    }
+require __DIR__ . '/vendor/autoload.php';
 
-    #frmContact div label {
-        margin-left: 5px
-    }
+$analyze = new TextAnalyzer\Analyzer();
+$analyzeFile = new TextAnalyzer\FileUpload();
+$analyzeByUrl = new TextAnalyzer\ExternalContent();
 
-    .demoInputBox {
-        padding: 10px;
-        border: #F0F0F0 1px solid;
-        border-radius: 4px;
-    }
+$error = '';
+$url = $_POST['text_url'] ?? '';
 
-    .error {
-        background-color: #FF6600;
-        border: #AA4502 1px solid;
-        padding: 5px 10px;
-        color: #FFFFFF;
-        border-radius: 4px;
+if (isset($_POST["submit_file"])) {
+    $text = $analyzeFile->upload();
+    if (!$text) {
+        $error = "<p class='error'>File type is not allowed!</p>";
     }
+} elseif (isset($_POST["submit_url"]) && $url) {
+    $text = $analyzeByUrl->getContent($url);
+    if (!$text) {
+        $error = "<p class='error'>Not a valid URL!</p>";
+    }
+} else {
+    $text = $_POST['text'] ?? '';
+}
 
-    .success {
-        background-color: #12CC1A;
-        border: #0FA015 1px solid;
-        padding: 5px 10px;
-        color: #FFFFFF;
-        border-radius: 4px;
-    }
+$text = trim($text);
+$time = date("Y-m-d H:i:s");
 
-    .info {
-        font-size: .8em;
-        color: #FF6600;
-        letter-spacing: 2px;
-        padding-left: 5px;
-    }
+$textHash = $analyze->calculateHash($text);
 
-    .btnAction {
-        background-color: #2FC332;
-        border: 0;
-        padding: 10px 40px;
-        color: #FFF;
-        border: #F0F0F0 1px solid;
-        border-radius: 4px;
-    }
-</style>
-<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
-<script>
-    function sendContact() {
-        var valid;
-        valid = validateContact();
-        if (valid) {
-            jQuery.ajax({
-                url: "ajax.php",
-                data: 'content=' + $("#content").val(),
-                type: "POST",
-                success: function (data) {
-                    $("#mail-status").html(data);
-                },
-                error: function () {
-                }
-            });
-        }
-    }
+$analyze_results = isset($_COOKIE[$textHash]) ? json_decode($_COOKIE[$textHash], true) : [
+    [
+        'title' => 'Number of characters:',
+        'result' => $analyze->numberOfCharacters($text),
+    ],
+    [
+        'title' => 'Number of words',
+        'result' => $analyze->numberOfWords($text),
+    ],
+    [
+        'title' => 'Number of sentences',
+        'result' => $analyze->numberOfSentences($text),
+    ],
+    [
+        'title' => 'Frequency of characters / Distribution of characters as a percentage of total',
+        'result' => $analyze->frequencyOfCharacters($text),
+    ],
+    [
+        'title' => 'Average word length',
+        'result' => sprintf(
+            "The average word length is %.02f characters",
+            $analyze->averageWordLength($text)
+        ),
+    ],
+    [
+        'title' => 'The average number of words in a sentence',
+        'result' => sprintf(
+            "The average number of words in a sentence %.02f",
+            $analyze->averageNumberOfWordsInSentence($text)
+        ),
+    ],
+    [
+        'title' => 'Top 10 most used words',
+        'result' => htmlspecialchars($analyze->mostUsedWords($text, 10)),
+    ],
+    [
+        'title' => 'Top 10 longest words',
+        'result' => htmlspecialchars($analyze->mostLongestShortestWords($text, 'long', 10)),
+    ],
+    [
+        'title' => 'Top 10 shortest words',
+        'result' => htmlspecialchars($analyze->mostLongestShortestWords($text, 'short', 10)),
+    ],
+    [
+        'title' => 'Top 10 longest sentences',
+        'result' => htmlspecialchars($analyze->mostLongestShortestSentences($text, 'long', 10)),
+    ],
+    [
+        'title' => 'Top 10 shortest sentences',
+        'result' => htmlspecialchars($analyze->mostLongestShortestSentences($text, 'short', 10)),
+    ],
+    [
+        'title' => 'Number of palindrome words',
+        'result' => $analyze->numberOfPalindromeWords($text),
+    ],
+    [
+        'title' => 'Top 10 longest palindrome words',
+        'result' => htmlspecialchars($analyze->mostLongestPalindromeWords($text, 'long', 10)),
+    ],
+    [
+        'title' => 'Is the whole text a palindrome? (Without whitespaces and punctuation marks)',
+        'result' => $analyze->isWholeTextPalindrome($text),
+    ],
+    [
+        'title' => 'The reversed text',
+        'result' => htmlspecialchars($analyze->reversedText($text)),
+    ],
+    [
+        'title' => 'The reversed text but the character order in words kept intact',
+        'result' => htmlspecialchars($analyze->mirrorMultibyteString($text)),
+    ],
+    [
+        'title' => 'The time it took to process the text in ms',
+        'result' => (microtime(TRUE) - $_SERVER['REQUEST_TIME_FLOAT']) * 1000 ?: 'not calculated because of 0ms',
+    ],
+    [
+        'title' => 'Hash',
+        'result' => $textHash,
+    ],
+];
 
-    function validateContact() {
-        var valid = true;
-        $(".demoInputBox").css('background-color', '');
-        $(".info").html('');
-        if (!$("#content").val()) {
-            $("#content-info").html("(required)");
-            $("#content").css('background-color', '#FFFFDF');
-            valid = false;
-        }
+// setting cookie
+if (!isset($_COOKIE[$textHash]) && !empty($text)) {
+    $result = json_encode($analyze_results);
+    setcookie($textHash, $result, time() + (86400 * 30), "/", null, true, true);
+}
 
-        return valid;
-    }
-</script>
-<div id="frmContact">
-    <div id="mail-status"></div>
-    <div>
-        <label>Content</label>
-        <span id="content-info" class="info"></span><br/>
-        <textarea name="content" id="content" class="demoInputBox" cols="60" rows="6"></textarea>
-    </div>
-    <div>
-        <button name="submit" class="btnAction" onClick="sendContact();">Send to validate</button>
-    </div>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Text Analyzer</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+
+<body>
+
+<div class="header">
+    <h2>Text Analyzer</h2>
 </div>
 
-<?php require_once 'form_file/file-uploader.php';
+<div class="main">
+    <?= $error ?>
+    <form action="index.php" method="POST">
+        <textarea type="text" name="text" rows="8"
+                  placeholder="Enter text to analyze"><?= htmlspecialchars($text); ?></textarea>
+        <br/>
+        <input type="submit" value="analyze text">
+    </form>
+    <br/><br/>
+    <form action="index.php" method="POST" enctype="multipart/form-data">
+        <br>
+        <input type="file" name="file" id="file">
+        <input type="submit" value="Submit" name="submit_file">
+        <p>Supported files: TXT, XML, HTML</p>
+    </form>
+    <br/><br/>
+    <form action="index.php" method="POST">
+        <input type="text" name="text_url" size="50" value="<?= htmlspecialchars($url); ?>">
+        <input type="submit" name="submit_url" value="get content via url">
+    </form>
+</div>
+
+<?php if ($text): ?>
+    <div class="results">
+        <h3>Statistical information</h3>
+
+        <?php foreach ($analyze_results as $result): ?>
+            <div class="result_item">
+                <div class="result_title"><?= $result['title']; ?></div>
+                <div class="result_data"><?= $result['result'] ?: " - no results -" ?></div>
+            </div>
+        <?php endforeach ?>
+        <div class="report">
+            Report was generated: <?= $time; ?>
+        </div>
+        <div class="report">
+            <form action="load-report.php" method="POST">
+                <input type="hidden" name="analyzed_text" value="<?= base64_encode(json_encode($analyze_results)); ?>">
+                <select name="export">
+                    <option value="csv">CSV</option>
+                    <option value="xml">XML</option>
+                    <option value="xlsx">XLSX</option>
+                </select>
+                <input type="submit" name="submit_report" value="Download Report">
+            </form>
+        </div>
+    </div>
+<?php endif; ?>
+
+</body>
+</html>
+
---
+++ HW8-Improve-Text-Analyzer/tests/AverageWordLengthTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class AverageWordLengthTest extends TestCase
+{
+
+    /**
+     * @dataProvider textDataProvider
+     *
+     * @param $input
+     * @param $expected
+     */
+    public function testFunction($input, $expected)
+    {
+        $analyze = new TextAnalyzer\Analyzer();
+        $this->assertEquals($expected, $analyze->averageWordLength($input));
+        $this->assertIsNumeric($analyze->averageWordLength($input));
+    }
+
+    public function textDataProvider()
+    {
+        return [
+            ['текст', 5],
+            ['текст для тесту', 4.33],
+            ['текст для, тесту', 4.33],
+            ['текст для, тесту.', 4.33],
+        ];
+    }
+
+}
